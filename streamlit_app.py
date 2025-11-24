85a07a0e1cdfc183890edc6fd5039eeabd1465e0
pradnyabambulkar <pradnyabambulkar@rediffmail.com>
2025-11-24 20:16:09 +0530

    Update streamlit_app.py
    error correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 #convert datafrome into pandas
-pd_df=my_data_frame.to_pandas()
+pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
